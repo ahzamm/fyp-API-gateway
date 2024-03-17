7bb13c5ded31dd5a54dc952fd4e1ea265efcbf57
ahzam <ahzamahmed6@gmail.com>
2024-03-17 16:42:00 +0500

    add user auth in home

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def signin():
     if request.method == "GET":
         token = request.cookies.get("token")
         if token:
-            url = "http://localhost:8000/api/user/profile"
+            url = "http://localhost:8001/api/user/profile"
             headers = {
                 "Content-Type": "application/json",
                 "Accept": "application/json",
@@ -78,7 +78,7 @@ def signin():
             "password": password,
         }
 
-        url = "http://localhost:8000/api/user/login"
+        url = "http://localhost:8001/api/user/login"
         headers = {"Content-Type": "application/json", "Accept": "application/json"}
         response = requests.post(url, json=data, headers=headers)
         response_data = response.json()
@@ -92,28 +92,27 @@ def signin():
             return render_template("signin.html", message=message)
 
 
-# @app.route("/home", methods=["GET", "POST"])
-# def home():
-#     if request.method == "GET":
-#         token = request.cookies.get("token")
-#         if token:
-#             url = "http://localhost:8000/api/user/profile"
-#             headers = {
-#                 "Content-Type": "application/json",
-#                 "Accept": "application/json",
-#                 "Authorization": "Bearer " + token,
-#             }
-#             response = requests.get(url, headers=headers)
-#             response_data = response.json()
-#             if response_data.get("success") == True:
-#                 return render_template("home.html")
-#         return redirect("signin")
-
-
 @app.route("/home", methods=["GET", "POST"])
 def home():
+    token = request.cookies.get("token")
+    if token is None:
+        return redirect("/signin")
+
+    url = "http://localhost:8001/api/user/profile"
+    headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Authorization": "Bearer " + token,
+    }
+    response = requests.get(url, headers=headers)
+    response_data = response.json()
+    if response_data.get("success") == True:
+        user_id = response_data.get("user").get("id")
+    else:
+        return redirect("/signin")
+
     if request.method == "GET":
-        url = " http://localhost:5002/retrieve-all-photos/?user_id=1"
+        url = f"http://localhost:5002/retrieve-all-photos/?user_id={user_id}"
         response = requests.get(url)
         images_base64 = response.json()
         images_data_urls = []
@@ -130,7 +129,7 @@ def home():
                 files = {"image": (filename, file)}
                 url = "http://127.0.0.1:5001/photos"
                 data = {
-                    "user_id": 1,
+                    "user_id": user_id,
                 }
                 response = requests.post(url, data=data, files=files)
                 vector_id = response.json().get("vector_id")
@@ -141,7 +140,7 @@ def home():
                 data = {
                     "vector_id": vector_id,
                     "filename": filename,
-                    "user_id": 1,
+                    "user_id": user_id,
                 }
                 response = requests.post(url, data=data, files=files)
                 return redirect("/home")
@@ -153,7 +152,7 @@ def home():
                 "Content-Type": "application/json",
                 "Accept": "application/json",
             }
-            data = {"query": query, "user_id": 1}
+            data = {"query": query, "user_id": user_id}
             response = requests.post(url, data=json.dumps(data), headers=headers)
             image_ids = response.json().get("image_ids")
 
@@ -165,6 +164,63 @@ def home():
                 image_data_url = "data:image/jpeg;base64," + image_base64
                 images_data_urls.append(image_data_url)
             return render_template("home.html", images=images_data_urls)
+
+
+# @app.route("/home", methods=["GET", "POST"])
+# def home():
+#     if request.method == "GET":
+#         url = " http://localhost:5002/retrieve-all-photos/?user_id=1"
+#         response = requests.get(url)
+#         images_base64 = response.json()
+#         images_data_urls = []
+#         for image_base64 in images_base64:
+#             image_data_url = "data:image/jpeg;base64," + image_base64
+#             images_data_urls.append(image_data_url)
+#         return render_template("home.html", images=reversed(images_data_urls))
+
+#     if request.method == "POST":
+#         if "image" in request.files:
+#             file = request.files["image"]
+#             if file:
+#                 filename = secure_filename(file.filename)
+#                 files = {"image": (filename, file)}
+#                 url = "http://127.0.0.1:5001/photos"
+#                 data = {
+#                     "user_id": 1,
+#                 }
+#                 response = requests.post(url, data=data, files=files)
+#                 vector_id = response.json().get("vector_id")
+
+#                 file.seek(0)
+#                 files = {"image": (filename, file)}
+#                 url = "http://127.0.0.1:5002/photos"
+#                 data = {
+#                     "vector_id": vector_id,
+#                     "filename": filename,
+#                     "user_id": 1,
+#                 }
+#                 response = requests.post(url, data=data, files=files)
+#                 return redirect("/home")
+
+#         query = request.form["search"]
+#         if query:
+#             url = "http://127.0.0.1:5001/query"
+#             headers = {
+#                 "Content-Type": "application/json",
+#                 "Accept": "application/json",
+#             }
+#             data = {"query": query, "user_id": 1}
+#             response = requests.post(url, data=json.dumps(data), headers=headers)
+#             image_ids = response.json().get("image_ids")
+
+#             url = f"http://127.0.0.1:5002/retrieve-photos/?vector_ids={image_ids}"
+#             response = requests.get(url)
+#             images_base64 = response.json()
+#             images_data_urls = []
+#             for image_base64 in images_base64:
+#                 image_data_url = "data:image/jpeg;base64," + image_base64
+#                 images_data_urls.append(image_data_url)
+#             return render_template("home.html", images=images_data_urls)
 
 
 if __name__ == "__main__":
